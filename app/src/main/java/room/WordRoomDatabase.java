bfcbe283b6f5b76472ff2a895fc1ae313974d15c
Smyejfatimaezzahra <fatimaezzahra.smyej@edu.uca.ma>
2022-05-16 20:14:30 +0000

    Initial commit

---
+++ app/src/main/java/room/WordRoomDatabase.java
@@ -0,0 +1,46 @@
+package room;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import beans.Word;
+import dao.WordDao;
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+    public abstract WordDao wordDao();
+    private static WordRoomDatabase INSTANCE;
+
+    public static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            // Wipes and rebuilds instead of migrating
+                            // if no Migration object.
+                            // Migration is not part of this practical.
+                            .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
